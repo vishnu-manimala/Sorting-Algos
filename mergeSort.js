1e3c7bd9502d62cdb5ce56155293e7ec1467a382
Vishnu Manimala <71614068+vishnu-manimala@users.noreply.github.com>
2023-04-18 10:32:10 +0530

    Add files via upload

---
+++ mergeSort.js
@@ -0,0 +1,36 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+      return arr;
+    }
+  
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    const result = [];
+  
+    while (left.length && right.length) {
+      if (left[0] <= right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+  
+    while (left.length) {
+      result.push(left.shift());
+    }
+  
+    while (right.length) {
+      result.push(right.shift());
+    }
+  
+    return result;
+  }
+  const unsortedArray = [5, 2, 4, 6, 1, 3];
+  let sorted = mergeSort(unsortedArray);
+  console.log(sorted);   
